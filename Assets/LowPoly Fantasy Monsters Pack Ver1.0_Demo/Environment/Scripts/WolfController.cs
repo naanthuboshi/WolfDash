017ae3f74fb598af42f661d85c2165bf5dc53e51
山口奈々 <7stars.0296@gmail.com>
2018-02-17 17:28:13 +0900

    オリジナルゲーム
    作成途中です。

---
+++ Assets/LowPoly Fantasy Monsters Pack Ver1.0_Demo/Environment/Scripts/WolfController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	//animatorコンポネートを取得
+	this.myAnimator=GetComponennt<Animator>();
+	//走るアニメーションを開始
+	this.myAnimator.SetFloat("Speed",1);
+	// Update is called once per frame
+	void Update () {
+	}
+}
