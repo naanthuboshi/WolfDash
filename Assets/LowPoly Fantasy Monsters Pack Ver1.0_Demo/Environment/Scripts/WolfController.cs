2e84dd73f05683331f52ad9b0f1427a874e31d40
山口奈々 <7stars.0296@gmail.com>
2018-02-18 15:24:25 +0900

    追加
    追加分です。

--- Assets/LowPoly Fantasy Monsters Pack Ver1.0_Demo/Environment/Scripts/WolfController.cs
+++ Assets/LowPoly Fantasy Monsters Pack Ver1.0_Demo/Environment/Scripts/WolfController.cs
@@ -1,39 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WolfController : MonoBehaviour {
+public class WolfController : MonoBehaviour
+{
 	//アニメーションするためのコンポーネントを入れる
 	Animator myAnimator;
 	//狼を移動するためのコンポーネントを入れる追加
 	private Rigidbody myRightbody;
 	//前進するための力（追加）
-	private float forwardForce=800.0f;
+	private float forwardForce = 800.0f;
 	//左右に移動するための力（追加）
-	private float turnForce=500.0f;
-    //左右の移動できる範囲（追加）
-	private float movableRange=3.4f;
+	private float turnForce = 500.0f;
+	//左右の移動できる範囲（追加）
+	private float movableRange = 3.4f;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		//animatorコンポネートを取得
 		this.myAnimator = GetComponent<Animator>();
 		//走るアニメーションを開始
 		this.myAnimator.SetBool("run", true);
 		//Rightbodyコンポーネントを取得（追加）
-		this.myRightbody=GetComponent<Rigidbody>();
+		this.myRightbody = GetComponent<Rigidbody>();
 	}
-	// Update is called once per frame
-	void Update () {
-		//狼に全方向の力を加える（追加）
-		this.myRightbody.AddForce(this.transform.forward*this.forwardForce);
-		//狼を矢印キーまたはボタンに応じて左右に移動させる(追加)
-		if (Input.GetKey (KeyCode.LeftArrow) &this.transform.position.x< -this.movableRange) 
-			//左に移動（追加)
-			this.myRightbody.AddForce (-this.turnForce,0, 0);
-		else if(Input.GetKey(KeyCode.RightArrow)&&this.transform.position.x<this.movableRange){
-	 //右に移動（追加）
-			this.myRightbody.AddForce(this.turnForce,0,0);
 
+	// Update is called once per frame
+	void Update()
+	{
+		//狼に全方向の力を加える（追加）
+		this.myRightbody.AddForce(this.transform.forward * this.forwardForce);
+		//狼を矢印キーまたはボタンに応じて左右に移動させる(追加)
+		if (Input.GetKey(KeyCode.LeftArrow)) // & this.transform.position.x < -this.movableRange)
+		{
+			//左に移動（追加)
+			this.myRightbody.AddForce(-this.turnForce, 0, 0);
 		}
+		else if (Input.GetKey(KeyCode.RightArrow)) // && this.transform.position.x < this.movableRange)
+		{
+			//右に移動（追加）
+			this.myRightbody.AddForce(this.turnForce, 0, 0);
 		}
-		}
+	}
+}
